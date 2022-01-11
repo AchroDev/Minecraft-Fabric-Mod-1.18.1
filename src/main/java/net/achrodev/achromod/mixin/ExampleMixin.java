930b3dc3d7ff73f11f9432471affeb6248d4c2c7
Achro <ztblaine@protonmail.com>
2022-01-11 13:29:08 -0600

    Set base config

---
+++ src/main/java/net/achrodev/achromod/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.achrodev.achromod.mixin;
+
+import net.achrodev.achromod.AchroMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		AchroMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
